1a7c2eb009522acd3a49820763fbdbb81c7e1798
Vovanus <54627796+VladimirFunnyDevelop@users.noreply.github.com>
2019-09-27 18:06:51 +0700

    added popup

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ let soundLink = $("#sound-link")[0];
         soundLink.pause();
         document.getElementById("theImage").style.visibility = "hidden";
     });
+
+    // popup
+
+    let popup = document.getElementById('mypopup'),
+        popupClose = document.querySelector('.close');
+
+        window.onload = function() {
+            popup.style.display = 'block';
+        }
+
+        popupClose.onclick = function() {
+            popup.style.display = 'none';
+        }
+
+    // popup close
